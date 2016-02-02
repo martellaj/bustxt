efadd757887419b48b87906d016f503aac455ca5
Joe Martella <martellaj@live.com>
2016-02-02 15:14:16 -0800

    :customs: Adds logging to server.

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
+    console.log('>>> A request was made to the server.');
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
--- source/server.ts
+++ source/server.ts
@@ -1,8 +1,9 @@
 import http = require('http');
-
 let port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
+  console.log('>>> A request was made to the server.');
+  
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   });
