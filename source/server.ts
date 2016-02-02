8f48b517cb7b9a9735285edb44feaee7bd8d5761
Joe Martella <martellaj@live.com>
2016-02-02 12:31:25 -0800

    :hatching_chick: Initial commit.

---
+++ source/server.ts
@@ -0,0 +1,10 @@
+import http = require('http');
+
+let port = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Hello, world!\n');
+}).listen(port);
