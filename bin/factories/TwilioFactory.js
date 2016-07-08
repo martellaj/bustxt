715bf99e0640565880c94ad32c50e1f14290aa40
Joe Martella <martellaj@live.com>
2016-07-08 16:41:45 -0700

    Formats SMS message to send Shannon.

--- bin/factories/TwilioFactory.js
+++ bin/factories/TwilioFactory.js
@@ -1,31 +1,34 @@
 var Config = require('../config');
-var moment = require('moment');
-var momentTimezone = require('moment-timezone');
+var moment = require('moment-timezone');
 var twilio = require('twilio');
 var client = twilio(Config.TWILIO_ACCOUNT_SID, Config.TWILIO_API_KEY);
 function sendSmsMessage(busData) {
     var messageBody = formatMessageText(busData);
-    // client.sendMessage({
-    //   to: Config.TO_NUMBER,
-    //   from: Config.FROM_NUMBER,
-    //   body: messageBody
-    // }, function(error, response) {
-    //   if (error) {
-    //     // TODO: Handle error.
-    //     return;
-    //   }
-    //   // TODO: Log successful SMS sent.
-    //   console.log('Successfully texted about a bus.');
-    // });
+    client.sendMessage({
+        to: Config.TO_NUMBER,
+        from: Config.FROM_NUMBER,
+        body: messageBody
+    }, function (error, response) {
+        if (error) {
+            // TODO: Handle error.
+            return;
+        }
+        // TODO: Log successful SMS sent.
+        console.log('Successfully texted about a bus.');
+    });
 }
 exports.sendSmsMessage = sendSmsMessage;
 function formatMessageText(busData) {
     var message = "";
-    var isEarly = busData.msToArrival < 0;
-    var minutesToArrival = isEarly ? Math.round((-1 * busData.msToArrival) / 60000) : Math.round(busData.msToArrival / 60000);
-    var arrivalTime = moment(busData.arrivalDateTime);
-    var arrivalTimePst = momentTimezone.tz(arrivalTime, 'PST');
-    message = "";
+    var minutesToArrival = Math.round(busData.msToArrival / 60000);
+    var arrivalTimePst = moment(busData.arrivalDateTime).tz('America/Los_Angeles').format('h:mma');
+    var isPredicted = busData.isPredicted;
+    if (isPredicted) {
+        message = "Your bus is " + minutesToArrival + " minutes away. Be at the bus stop by " + arrivalTimePst + "!";
+    }
+    else {
+        message = "Your bus doesn't have tracking info, but based on the schedule, it's " + minutesToArrival + " away. Be at the bus stop by " + arrivalTimePst + "!";
+    }
     return message;
 }
 //# sourceMappingURL=TwilioFactory.js.map
--- source/factories/TwilioFactory.ts
+++ source/factories/TwilioFactory.ts
@@ -1,7 +1,6 @@
 import Config = require('../config');
 import { IArrivalTimeResponse } from '../models/IArrivalTimeResponse';
-import moment = require('moment');
-import momentTimezone = require('moment-timezone');
+import moment = require('moment-timezone');
 
 let twilio = require('twilio');
 let client = twilio(Config.TWILIO_ACCOUNT_SID, Config.TWILIO_API_KEY);
@@ -9,28 +8,32 @@ let client = twilio(Config.TWILIO_ACCOUNT_SID, Config.TWILIO_API_KEY);
 export function sendSmsMessage (busData: IArrivalTimeResponse) {
   let messageBody = formatMessageText(busData);
 
-  // client.sendMessage({
-  //   to: Config.TO_NUMBER,
-  //   from: Config.FROM_NUMBER,
-  //   body: messageBody
-  // }, function(error, response) {
-  //   if (error) {
-  //     // TODO: Handle error.
-  //     return;
-  //   }
+  client.sendMessage({
+    to: Config.TO_NUMBER,
+    from: Config.FROM_NUMBER,
+    body: messageBody
+  }, function (error, response) {
+    if (error) {
+      // TODO: Handle error.
+      return;
+    }
 
-  //   // TODO: Log successful SMS sent.
-  //   console.log('Successfully texted about a bus.');
-  // });
+    // TODO: Log successful SMS sent.
+    console.log('Successfully texted about a bus.');
+  });
 }
 
 function formatMessageText (busData: IArrivalTimeResponse): string {
-  let message = ``;
-  let isEarly: boolean = busData.msToArrival < 0;
-  let minutesToArrival: number = isEarly ? Math.round((-1 * busData.msToArrival) / 60000) : Math.round(busData.msToArrival / 60000);
-  let arrivalTime = moment(busData.arrivalDateTime);
-  let arrivalTimePst = momentTimezone.tz(arrivalTime, 'PST');
+  let message: string = ``;
+  let minutesToArrival: number = Math.round(busData.msToArrival / 60000)
+  let arrivalTimePst: string = moment(busData.arrivalDateTime).tz('America/Los_Angeles').format('h:mma');
+  let isPredicted: boolean = busData.isPredicted;
 
-  message = ``;
+  if (isPredicted) {
+    message = `Your bus is ${minutesToArrival} minutes away. Be at the bus stop by ${arrivalTimePst}!`;
+  } else {
+    message = `Your bus doesn't have tracking info, but based on the schedule, it's ${minutesToArrival} away. Be at the bus stop by ${arrivalTimePst}!`;
+  }
+
   return message;
 }
