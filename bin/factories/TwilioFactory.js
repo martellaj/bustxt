4eff422cfe56fb069aab05bb89a2290179ad19fb
Joe Martella <martellaj@live.com>
2016-07-07 21:27:27 -0700

    Adds moment libraries for SMS body formatting.

--- bin/factories/TwilioFactory.js
+++ bin/factories/TwilioFactory.js
@@ -1,23 +1,31 @@
 var Config = require('../config');
+var moment = require('moment');
+var momentTimezone = require('moment-timezone');
 var twilio = require('twilio');
 var client = twilio(Config.TWILIO_ACCOUNT_SID, Config.TWILIO_API_KEY);
 function sendSmsMessage(busData) {
-    client.sendMessage({
-        to: Config.TO_NUMBER,
-        from: Config.FROM_NUMBER,
-        body: formatMessageText(busData)
-    }, function (error, response) {
-        if (error) {
-            // TODO: Handle error.
-            return;
-        }
-        // TODO: Log successful SMS sent.
-        console.log('Successfully texted about a bus.');
-    });
+    var messageBody = formatMessageText(busData);
+    // client.sendMessage({
+    //   to: Config.TO_NUMBER,
+    //   from: Config.FROM_NUMBER,
+    //   body: messageBody
+    // }, function(error, response) {
+    //   if (error) {
+    //     // TODO: Handle error.
+    //     return;
+    //   }
+    //   // TODO: Log successful SMS sent.
+    //   console.log('Successfully texted about a bus.');
+    // });
 }
 exports.sendSmsMessage = sendSmsMessage;
 function formatMessageText(busData) {
-    var message = "Your bus will arrive in " + busData.msToArrival + " ms.";
+    var message = "";
+    var isEarly = busData.msToArrival < 0;
+    var minutesToArrival = isEarly ? Math.round((-1 * busData.msToArrival) / 60000) : Math.round(busData.msToArrival / 60000);
+    var arrivalTime = moment(busData.arrivalDateTime);
+    var arrivalTimePst = momentTimezone.tz(arrivalTime, 'PST');
+    message = "";
     return message;
 }
 //# sourceMappingURL=TwilioFactory.js.map
--- source/factories/TwilioFactory.ts
+++ source/factories/TwilioFactory.ts
@@ -1,26 +1,36 @@
 import Config = require('../config');
 import { IArrivalTimeResponse } from '../models/IArrivalTimeResponse';
+import moment = require('moment');
+import momentTimezone = require('moment-timezone');
 
 let twilio = require('twilio');
 let client = twilio(Config.TWILIO_ACCOUNT_SID, Config.TWILIO_API_KEY);
 
 export function sendSmsMessage (busData: IArrivalTimeResponse) {
-  client.sendMessage({
-    to: Config.TO_NUMBER,
-    from: Config.FROM_NUMBER,
-    body: formatMessageText(busData)
-  }, function(error, response) {
-    if (error) {
-      // TODO: Handle error.
-      return;
-    }
+  let messageBody = formatMessageText(busData);
 
-    // TODO: Log successful SMS sent.
-    console.log('Successfully texted about a bus.');
-  });
+  // client.sendMessage({
+  //   to: Config.TO_NUMBER,
+  //   from: Config.FROM_NUMBER,
+  //   body: messageBody
+  // }, function(error, response) {
+  //   if (error) {
+  //     // TODO: Handle error.
+  //     return;
+  //   }
+
+  //   // TODO: Log successful SMS sent.
+  //   console.log('Successfully texted about a bus.');
+  // });
 }
 
 function formatMessageText (busData: IArrivalTimeResponse): string {
-  let message = `Your bus will arrive in ${busData.msToArrival} ms.`;
+  let message = ``;
+  let isEarly: boolean = busData.msToArrival < 0;
+  let minutesToArrival: number = isEarly ? Math.round((-1 * busData.msToArrival) / 60000) : Math.round(busData.msToArrival / 60000);
+  let arrivalTime = moment(busData.arrivalDateTime);
+  let arrivalTimePst = momentTimezone.tz(arrivalTime, 'PST');
+
+  message = ``;
   return message;
 }
