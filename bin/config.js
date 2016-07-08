e6391064dbba144da0ed44e548e28d38e69a2543
Joe Martella <martellaj@live.com>
2016-07-08 16:49:35 -0700

    Updates config file to match Azure settings.

---
+++ bin/config.js
@@ -0,0 +1,6 @@
+exports.OBA_API_KEY = process.env.OBA_API_KEY;
+exports.TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+exports.TWILIO_API_KEY = process.env.TWILIO_API_KEY;
+exports.FROM_NUMBER = process.env.FROM_NUMBER;
+exports.TO_NUMBER = process.env.TO_NUMBER;
+//# sourceMappingURL=config.js.map
